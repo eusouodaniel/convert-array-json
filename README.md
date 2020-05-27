e39480a5601172c23984dae044930e385b5557ed
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-05-26 22:14:29 -0300

    NEW: First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# How to use?
+
+
+- JSON: <br>
+const json = {
+	"Teste": true,
+	"Teste2": false
+};<br>
+const  convert  =  require('convert-array-json');<br>
+.<br>
+.<br>
+convert.convertToJson(json);<br><br>
+
+- Array: <br>
+const array = [
+	"Teste",
+	"Teste2"
+]; <br>
+const  convert  =  require('convert-array-json');<br>
+.<br>
+.<br>
+convert.convertToArray(array);
+
+
+@eusouodaniel
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-module.exports.convertToJson = values => {
-    return JSON.stringify(values);
-};
+function convertToArray(values) {
+    var result = [];
+    for(var i in values)
+        result.push([i, values[i]]);
 
-module.exports = convertToArray = values => {
-    return Object.keys(values).map(i => JSON.parse(values[Number(i)]));
+    return JSON.stringify(result);
+}
+
+function convertToJson(values) {
+    return JSON.stringify(values);
+}
+
+module.exports = {
+    convertToArray,
+    convertToJson
 }
